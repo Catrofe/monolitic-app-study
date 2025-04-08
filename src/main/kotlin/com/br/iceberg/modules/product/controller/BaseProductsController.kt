029f208fe0ea4b12d7510d38e67ce51edca3fb7f
Catrofe <lord.rico10@gmail.com>
2025-04-07 23:38:20 -0300

    FEAT: Update deleteProduct endpoint to include path variable and set response status

--- src/main/kotlin/com/br/iceberg/modules/product/controller/BaseProductsController.kt
+++ src/main/kotlin/com/br/iceberg/modules/product/controller/BaseProductsController.kt
@@ -5,6 +5,7 @@ import com.br.iceberg.modules.product.dto.UpdateProductDto
 import com.br.iceberg.modules.product.service.BaseProductsService
 import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -38,6 +40,7 @@ class BaseProductsController(
     @GetMapping("/{id}")
     fun getProductById(@PathVariable id: Long) = baseProductsService.getProductById(id)
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteProduct(@PathVariable id: Long) = baseProductsService.deleteProduct(id)
 }
